715295ce4bb5188e2a619a4737a289ad734223cc
rmccormack <rmccormack@unilink.com>
2025-03-12 12:14:57 +0000

    OpenApiDocsTest fix

--- src/test/kotlin/uk/gov/justice/digital/hmpps/breachnoticeapi/integration/OpenApiDocsTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/breachnoticeapi/integration/OpenApiDocsTest.kt
@@ -66,10 +66,18 @@ class OpenApiDocsTest : IntegrationTestBase() {
   fun `the open api json path security requirements are valid`() {
     val result = OpenAPIV3Parser().readLocation("http://localhost:$port/v3/api-docs", null, null)
 
+    val exclusions = setOf(
+      "/queue-admin/retry-dlq/{dlqName}",
+      "/queue-admin/retry-all-dlqs",
+      "/queue-admin/purge-queue/{queueName}",
+      "/queue-admin/get-dlq-messages/{dlqName}",
+      "/subject-access-request",
+    )
+
     // The security requirements of each path don't appear to be validated like they are at https://editor.swagger.io/
     // We therefore need to grab all the valid security requirements and check that each path only contains those items
     val securityRequirements = result.openAPI.security.flatMap { it.keys }
-    result.openAPI.paths.filter { it.value.get != null }.forEach { pathItem ->
+    result.openAPI.paths.filter { !exclusions.contains(it.key) }.filter { it.value.get != null }.forEach { pathItem ->
       assertThat(pathItem.value.get.security.flatMap { it.keys }).isSubsetOf(securityRequirements)
     }
   }
@@ -94,12 +102,20 @@ class OpenApiDocsTest : IntegrationTestBase() {
 
   @Test
   fun `all endpoints have a security scheme defined`() {
+    // There are 4 SQS endpoints without security scheme, to be excluded from this test; all these endpoint has single tag "hmpps-queue-resource"
+    val queueAdminTag = "hmpps-queue-resource"
+    val subjectAccessTag = "Subject Access Request"
+    val excludedEndpointCount = 1
+
     webTestClient.get()
       .uri("/v3/api-docs")
       .accept(MediaType.APPLICATION_JSON)
       .exchange()
       .expectStatus().isOk
       .expectBody()
-      .jsonPath("$.paths[*][*][?(!@.security)]").doesNotExist()
+      .jsonPath("$.paths[*][*][?(!@.security)]..tags[0]").value<JSONArray> {
+        assertThat(it).hasSize(excludedEndpointCount)
+        it.forEach { tag -> assertThat(tag).isIn(queueAdminTag, subjectAccessTag) }
+      }
   }
 }
