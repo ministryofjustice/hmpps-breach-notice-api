84e869fd92e07de631eb88060d22b1d5545db645
karlfosterBCL <39554570+karlfosterBCL@users.noreply.github.com>
2025-07-03 15:30:54 +0100

    unit test extension config (#70)
    * unit test extension config
    
    * lint
    
    * reworked per test

--- src/test/kotlin/uk/gov/justice/digital/hmpps/breachnoticeapi/integration/PdfGenerationTests.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/breachnoticeapi/integration/PdfGenerationTests.kt
@@ -7,6 +7,7 @@ import org.springframework.http.ContentDisposition
 import org.springframework.http.MediaType
 import uk.gov.justice.digital.hmpps.breachnoticeapi.model.BreachNotice
 import uk.gov.justice.digital.hmpps.breachnoticeapi.repository.BreachNoticeRepository
+import java.time.Duration
 import java.util.UUID
 
 class PdfGenerationTests : IntegrationTestBase() {
@@ -16,7 +17,9 @@ class PdfGenerationTests : IntegrationTestBase() {
 
   @Test
   fun `get PDF should return a 200 response`() {
-    webTestClient.post()
+    webTestClient
+      .mutate().responseTimeout(Duration.ofSeconds(30)).build()
+      .post()
       .uri("/breach-notice")
       .headers(setAuthorisation(roles = listOf("ROLE_BREACH_NOTICE")))
       .bodyValue(
@@ -31,7 +34,9 @@ class PdfGenerationTests : IntegrationTestBase() {
     val breachNotice = breachNoticeRepository.findByCrn("X000007")
     assertThat(breachNotice.first().crn).isEqualTo("X000007")
 
-    webTestClient.get()
+    webTestClient
+      .mutate().responseTimeout(Duration.ofSeconds(30)).build()
+      .get()
       .uri("/breach-notice/" + breachNotice[0].id + "/pdf")
       .headers(setAuthorisation(roles = listOf("ROLE_BREACH_NOTICE")))
       .exchange()
