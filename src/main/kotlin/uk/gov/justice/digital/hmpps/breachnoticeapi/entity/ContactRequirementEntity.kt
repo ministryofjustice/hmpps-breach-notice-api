4c4cf2f63726fe81111ac77f7220be60f20c2c33
rmccormack <rmccormack@unilink.com>
2025-09-11 13:00:53 +0100

    BR-271 / BR-272 : ktlintFixes

--- src/main/kotlin/uk/gov/justice/digital/hmpps/breachnoticeapi/entity/ContactRequirementEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/breachnoticeapi/entity/ContactRequirementEntity.kt
@@ -1,13 +1,19 @@
 package uk.gov.justice.digital.hmpps.breachnoticeapi.entity
 
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
 import org.springframework.data.annotation.CreatedBy
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedBy
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
-import java.util.*
+import java.util.UUID
 
 @Entity
 @Table(name = "contact_requirement")
