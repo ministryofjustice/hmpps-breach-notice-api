e48a92dda5c86ec28f3d56511731707daa4a74b6
rmccormack <rmccormack@unilink.com>
2025-09-23 16:13:31 +0100

    BR-271 : Test fix

--- src/main/kotlin/uk/gov/justice/digital/hmpps/breachnoticeapi/controller/BreachNoticeRequirementController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/breachnoticeapi/controller/BreachNoticeRequirementController.kt
@@ -77,7 +77,7 @@ class BreachNoticeRequirementController(
     ],
   )
   fun updateBreachNoticeRequirement(
-    @RequestBody breachNoticeRequirement: BreachNoticeRequirement,
+    @PathVariable id: UUID, @RequestBody breachNoticeRequirement: BreachNoticeRequirement,
   ) = breachNoticeRequirementService.updateBreachNoticeRequirement(breachNoticeRequirement)
 
   @DeleteMapping("/unlinkedrequirements/{breachNoticeId}")
