3bb53b1d6113ef132fcaf193df19d63cb0a38bbd
rmccormack <rmccormack@unilink.com>
2026-08-11 11:39:56 +0100

    BR-360 : PDM to update address field limits

---
+++ src/main/resources/db/migration/V1_15__address_field_lengths.sql
@@ -0,0 +1,3 @@
+ALTER TABLE public.address ALTER COLUMN building_name TYPE varchar(80);
+ALTER TABLE public.address ALTER COLUMN street_name TYPE varchar(80);
+ALTER TABLE public.address ALTER COLUMN district TYPE varchar(80);
