b5ffdeb634ad74af75370ca737f22961fb3eb373
rmccormack <rmccormack@unilink.com>
2026-08-21 15:20:01 +0100

    BR-378 : Domain event datetime fields offset by 1hr

--- src/main/resources/db/migration/V1_16__review_and_terminated_date_type_change.sql
+++ src/main/resources/db/migration/V1_16__review_and_terminated_date_type_change.sql
@@ -1,2 +1,2 @@
-ALTER TABLE public.breach_notice ALTER column review_required_date type timestamp with time zone USING review_required_date AT TIME ZONE 'Europe/London',
+ALTER TABLE public.breach_notice ALTER column review_required_date type timestamp with time zone USING review_required_date AT TIME ZONE 'Europe/London';
 ALTER TABLE public.breach_notice ALTER column terminated_unterminated_date type timestamp with time zone USING terminated_unterminated_date AT TIME ZONE 'Europe/London';
