3c1f878a8a3147fb91753816c9b7c0e607b7bcda
peter wilson <pwilson@unilink.com>
2026-08-28 11:57:52 +0100

    Uplift SpringBoot to 11.06

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.5.4"
-  kotlin("plugin.spring") version "2.4.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "11.0.6"
+  kotlin("plugin.spring") version "2.4.10"
   kotlin("plugin.jpa") version "2.4.10"
   id("org.flywaydb.flyway") version "13.4.0"
 }
